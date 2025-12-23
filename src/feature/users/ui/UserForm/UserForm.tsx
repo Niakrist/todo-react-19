81480f265a180f0b0db35d2ca8e456162f3398a0
Niakrist <ak_88dron@inbox.ru>
2025-12-23 16:18:17 +0300

    feat: emplement api for add user and remove user

--- src/feature/users/ui/UserForm/UserForm.tsx
+++ src/feature/users/ui/UserForm/UserForm.tsx
@@ -1,26 +1,40 @@
-import { useState } from 'react'
+import { useState, useTransition } from 'react'
 import { createUser } from '../../../../shared/api/api'
 
 export const UserForm = ({ refetchUsers }: { refetchUsers: () => void }) => {
   const [email, setEmail] = useState('')
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const [isPending, startTransition] = useTransition()
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    await createUser({ email, id: crypto.randomUUID() })
-    refetchUsers()
-    setEmail('')
+
+    // Позволяет реализовать концепцию Stale-While-Revalidate (SWR)
+    // Transition в React нужен для долгих переходов (загрузки новых данных) , useState() в input - быстрый переход
+    startTransition(async () => {
+      await createUser({ email, id: crypto.randomUUID() })
+      startTransition(() => {
+        refetchUsers()
+        setEmail('')
+      })
+    })
   }
+
+  console.log('isPending: ', isPending)
 
   return (
     <form onSubmit={handleSubmit} className='flex gap-2'>
       <input
         onChange={e => setEmail(e.target.value)}
-        className='border p-2 rounded'
+        value={email}
+        className='border p-2 rounded disabled:bg-gray-500'
         type='email'
         required
+        disabled={isPending}
       />
       <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500'
         type='submit'
+        disabled={isPending}
       >
         Add
       </button>

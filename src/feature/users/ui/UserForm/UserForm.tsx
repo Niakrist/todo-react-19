0e7eba748da92ac7ff690436e815f5b7b10042ec
Niakrist <ak_88dron@inbox.ru>
2025-12-23 15:25:46 +0300

    feat: emplement api

--- src/feature/users/ui/UserForm/UserForm.tsx
+++ src/feature/users/ui/UserForm/UserForm.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+import { createUser } from '../../../../shared/api/api'
 
-export const UserForm = () => {
+export const UserForm = ({ refetchUsers }: { refetchUsers: () => void }) => {
+  const [email, setEmail] = useState('')
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await createUser({ email, id: crypto.randomUUID() })
+    refetchUsers()
+    setEmail('')
+  }
+
   return (
-    <form className='flex gap-2'>
-      <input className='border p-2 rounded' type='email' required />
+    <form onSubmit={handleSubmit} className='flex gap-2'>
+      <input
+        onChange={e => setEmail(e.target.value)}
+        className='border p-2 rounded'
+        type='email'
+        required
+      />
       <button
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
         type='submit'
--- src/feature/users/ui/UserList/UserList.tsx
+++ src/feature/users/ui/UserList/UserList.tsx
@@ -1,11 +1,18 @@
+import { use } from 'react'
 import type { UserType } from '../../model'
 import { UserCard } from './UserCard'
 
 type IUserListProps = {
-  users: UserType[]
+  usersPromise: Promise<UserType[]>
 }
 
-export const UserList = ({ users }: IUserListProps) => {
+export const UserList = ({ usersPromise }: IUserListProps) => {
+  // хук use прямо в рендере превращает наш Promise в users
+  // use выбрасывает ошибку с Promise если возвращаются не users, а пустой [] или ошибка
+  const users = use(usersPromise)
+
+  console.log('users: ', users)
+
   return (
     <ul className='flex flex-col'>
       {users.map(user => (

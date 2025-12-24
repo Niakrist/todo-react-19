0a626e73473f259b6465be0020165421352063bb
Niakrist <ak_88dron@inbox.ru>
2025-12-24 16:34:45 +0300

    Revert "Revert "Merge pull request #7 from Niakrist/feature/4""
    This reverts commit 307de906ae174554d92e3469b83bed23686a2b9d.

--- src/feature/users/ui/UserList/UserCard.tsx
+++ src/feature/users/ui/UserList/UserCard.tsx
@@ -1,17 +1,34 @@
+import { useTransition } from 'react'
+import { deleteUser } from '../../../../shared/api/api'
 import type { UserType } from '../../model'
 
 type IUserCardProps = {
   user: UserType
+  refetchUsers: () => void
 }
 
-export const UserCard = ({ user }: IUserCardProps) => {
+export const UserCard = ({ user, refetchUsers }: IUserCardProps) => {
+  // pisimistic update
+
+  const [isPending, startTransition] = useTransition()
+  const handleDelete = async (id: string) => {
+    startTransition(async () => {
+      await deleteUser(id)
+      startTransition(async () => {
+        refetchUsers()
+      })
+    })
+  }
+
   return (
     <li className='flex justify-between items-center border p-2 m-2 rounded bg-gray-100'>
       {user.email}
 
       <button
-        className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'
+        onClick={() => handleDelete(user.id)}
+        className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500'
         type='button'
+        disabled={isPending}
       >
         delete
       </button>
